12121e095d1dc04d32d9ea853f2038e66dcaec46
Alexander Courtis <acourtis@atlassian.com>
2017-05-03 23:26:49 +1000

    test-xrandrutil just extends XrrWrapper rather than an interface

--- src/xrandrrutil.h
+++ src/xrandrrutil.h
@@ -1,8 +1,40 @@
 #ifndef XLAYOUTDISPLAYS_XRANDRUTIL_H
 #define XLAYOUTDISPLAYS_XRANDRUTIL_H
 
-#include "XrrWrapper.h"
 #include "Displ.h"
+
+class XrrWrapper {
+public:
+    virtual ~XrrWrapper() {};
+
+    virtual Display *xOpenDisplay(_Xconst char *name) const {
+        return XOpenDisplay(name);
+    }
+
+    virtual int defaultScreen(Display *dpy) const {
+        return DefaultScreen(dpy);
+    }
+
+    virtual int screenCount(Display *dpy) const {
+        return ScreenCount(dpy);
+    }
+
+    virtual Window rootWindow(Display *dpy, int scr) const {
+        return RootWindow(dpy, scr);
+    }
+
+    virtual XRRScreenResources *xrrGetScreenResources(Display *dpy, Window window) const {
+        return XRRGetScreenResources(dpy, window);
+    }
+
+    virtual XRROutputInfo *xrrGetOutputInfo(Display *dpy, XRRScreenResources *resources, RROutput output) const {
+        return XRRGetOutputInfo(dpy, resources, output);
+    }
+
+    virtual XRRCrtcInfo *xrrGetCrtcInfo(Display *dpy, XRRScreenResources *resources, RRCrtc crtc) const {
+        return XRRGetCrtcInfo(dpy, resources, crtc);
+    }
+};
 
 // v refresh frequency in even Hz, zero if modeInfo is NULL
 const unsigned int refreshFromModeInfo(const XRRModeInfo &modeInfo);
@@ -16,7 +48,7 @@ const std::string renderCmd(const std::list<DisplP> &displs);
 // render a user readable string explaining the current state of displs
 const std::string renderUserInfo(const std::list<DisplP> &displs);
 
-// build a list of Displ based on the current and possible state of the world; override xrrWrapper for testing
-const std::list<DisplP> discoverDispls(const XrrWrapper *xrrWrapper = new XrrWrapperImpl());
+// build a list of Displ based on the current and possible state of the world; override xrrWrapper only for testing
+const std::list<DisplP> discoverDispls(const XrrWrapper *xrrWrapper = new XrrWrapper());
 
 #endif //XLAYOUTDISPLAYS_XRANDRUTIL_H
