8f9073b2b936020dc902c7b431a704d1dfc0c306
Alexander Courtis <alex@courtis.org>
2021-07-05 10:47:58 +1000

    hack to force inclusion of the static BPO instead of linking the dynamic (g++ preference)

--- config.mk
+++ config.mk
@@ -9,7 +9,8 @@ CPPFLAGS = $(INCS) -DVERSION=\"$(VERSION)\"
 
 CXXFLAGS = -pedantic -Wall -Wextra -Werror -O3 -std=c++14
 
-LDFLAGS = -lX11 -lXcursor -lXrandr -lboost_program_options
+# hack to force inclusion of the static BPO instead of linking the dynamic (g++ preference)
+LDFLAGS = /usr/lib/libboost_program_options.a -lX11 -lXcursor -lXrandr
 LDFLAGS_TEST = -lgmock -lgtest -pthread
 
 CXX = g++
